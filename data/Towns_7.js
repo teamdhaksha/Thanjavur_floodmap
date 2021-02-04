ed8b3f69ab1d2b54148e020a079c2331636128b3
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-02-04 11:50:16 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,13 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Adirampattinam" }, "geometry": { "type": "Point", "coordinates": [ 79.377116833097006, 10.343695369199899, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Pattukottai" }, "geometry": { "type": "Point", "coordinates": [ 79.313937774735891, 10.42795549551701, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Madukkur" }, "geometry": { "type": "Point", "coordinates": [ 79.398592797266076, 10.48141623063789, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thanjavur" }, "geometry": { "type": "Point", "coordinates": [ 79.134043770772237, 10.791365858736951, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thirukattupalli" }, "geometry": { "type": "Point", "coordinates": [ 78.954221944520739, 10.85046314072121, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kumbakonam" }, "geometry": { "type": "Point", "coordinates": [ 79.377292653309198, 10.95963741853663, -999999.0 ] } }
+]
+}
